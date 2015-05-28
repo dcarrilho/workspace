dc9b40fe6edd4f364ea9298ea28a4fba4edc9a0c
dcarrilho <diego_carrilho@hotmail.com>
2015-05-28 07:43:09 -0400

    criacao do projeto SisAuto

---
+++ SisAuto/src/main/java/br/facom/apsoo/sisauto/App.java
@@ -0,0 +1,13 @@
+package br.facom.apsoo.sisauto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
